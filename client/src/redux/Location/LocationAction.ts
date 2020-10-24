9c3177d2e1b99aaa4d9ffc3c20a8860265dccf3c
lequeston <lequeston@yandex.ru>
2020-10-24 17:03:53 +0300

    Сделал парс элементов

--- client/src/redux/Location/LocationAction.ts
+++ client/src/redux/Location/LocationAction.ts
@@ -5,8 +5,8 @@ import { FetchLocation } from "./LocationTypings";
 export const getUserPosition = (): FetchLocation => {
   return async (dispatch) => {
     try {
-      const responce = await fetch(URL_POSITION);
-      const body = await responce.json();
+      const response = await fetch(URL_POSITION);
+      const body = await response.json();
       console.log(body);
     } catch(e) {
       console.log(e);
